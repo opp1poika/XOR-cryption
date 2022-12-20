490afead2fc3b7fac4e9645eefe0db8908f43801
opp1poika <119451400+opp1poika@users.noreply.github.com>
2022-12-20 16:20:34 +0200

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,1 +1,37 @@
+def crypt():
+    L = []
+    print("Insert key >>", end="")
+    key = input()
+    print("Insert text>>", end="")
+    text = input()
+    print("Crypted text<<", end="")
+    k = len(key)
+    t = len(text)
+    for i in range(t):
+        L.append(ord(text[i]) ^ ord(key[i % k]))
+    print(L)
+    
+def encrypt():
+    L = []
+    print("Insert key >>", end="")
+    key = input()
+    print("Insert list>>", end="")
+    nlist = list(map(int, input().split()))
+    k = len(key)
+    n = len(nlist)
+    for i in range(n):
+        L.append(chr(nlist[i] ^ ord(key[i % k])))
+    print("Encrypted text<<", end="")
+    print(*L, sep="")
 
+def main():
+    print("Crypt text (1)\nEncrypt text (2)\n>>", end="")
+    function = input()
+    
+    if function == "1":
+        crypt()
+    if function == "2":
+        encrypt()
+    main()
+
+main()
